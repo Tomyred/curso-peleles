79d868d4dcc838b71995142548672993a876ecde
Tomyred <tomy.rojo.1@gmail.com>
2022-05-03 20:59:18 -0300

    add user to list

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const title = document.querySelector("#title");
 const button = document.querySelector("#myButton");
 const input = document.querySelector("#myInput");
+const list = document.querySelector(".userList");
 
-button.addEventListener("click", function () {
-    input.value = "";
-});
+const users = ["Juancito", "John Doe", "Pedro"];
 
 input.addEventListener("input", function (e) {
     const inputText = e.target.value;
     title.innerHTML = inputText;
 });
+
+button.addEventListener("click", function () {
+    if (input.value.length === 0) {
+        return;
+    }
+    users.push(input.value);
+    list.innerHTML += `<li>${input.value}</li>`;
+    input.value = "";
+});
+
+users.forEach(function (user) {
+    list.innerHTML += `<li>${user}</li>`;
+});
+
+const string = "string";
